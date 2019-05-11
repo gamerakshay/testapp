8cd1e4d4db1d984c9f568797857fc5f2861791dd
gamerakshay <48151925+gamerakshay@users.noreply.github.com>
2019-05-11 11:37:45 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.testapp;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button open = (Button)findViewById(R.id.open);
+        open.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWifiSettings();
+            }
+        });
+    }
+
+    public void openWifiSettings(){
+
+        final Intent intent = new Intent(Intent.ACTION_MAIN, null);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+        final ComponentName cn = new ComponentName("com.android.settings", "com.android.settings.wifi.WifiSettings");
+        intent.setComponent(cn);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity( intent);
+    }
+}
